ee3d11c55c7e54fc75b28379174addb83516f3b1
Christopher Bandurski <cbandurski1@gmail.com>
2023-12-01 16:37:18 -0600

    removed router

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,58 @@
 <script setup lang="ts">
-import { useRoute } from 'vue-router';
+import GameMenu from '@/components/menus/GameMenu.vue';
+import HintMenu from '@/components/menus/HintMenu.vue';
+import SudokuControls from '@/components/sudoku/SudokuControls.vue';
+import SudokuGrid from '@/components/sudoku/SudokuGrid.vue';
+import { useGlobalInputs } from '@/composables/globalInputs';
+import { useSudokuStore } from '@/stores/sudoku';
 
-const route = useRoute();
+const sudokuStore = useSudokuStore();
+const { newGame } = sudokuStore;
+
+useGlobalInputs();
+newGame('easy');
 </script>
 
 <template>
-  <RouterView />
+  <div class="wrapper">
+    <main>
+      <aside class="game-menu">
+        <GameMenu />
+      </aside>
+      <div></div>
+      <section>
+        <SudokuGrid />
+      </section>
+      <aside class="control-menu">
+        <HintMenu />
+        <SudokuControls />
+      </aside>
+    </main>
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped>
+.wrapper {
+  display: flex;
+  justify-content: center;
+  user-select: none;
+}
+
+main {
+  padding: 50px;
+  display: grid;
+  grid-template-columns: auto 296px;
+  gap: 20px;
+}
+
+.control-menu {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding-block: 5px;
+}
+
+.hint-message {
+  text-align: center;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import PrimeVue from 'primevue/config';
 import 'primevue/resources/themes/lara-light-blue/theme.css';
 import Tooltip from 'primevue/tooltip';
 
-import { createRouter } from '@/router';
 import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 import App from './App.vue';
@@ -12,7 +11,6 @@ import App from './App.vue';
 const app = createApp(App);
 
 app.use(createPinia());
-app.use(createRouter());
 app.use(PrimeVue);
 app.directive('tooltip', Tooltip);
 
